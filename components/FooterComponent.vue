b8567e6fb686f8d5f9666080070b98e7a3bc9df7
Dikatso Ramatsela <dikatsoramatsela@Dikatsos-MacBook-Pro.local>
2024-08-18 10:46:14 -0400

    footer updates

---
+++ components/FooterComponent.vue
@@ -0,0 +1,216 @@
+<template>
+    <div class="container-fluid footer-wrapper">
+        <div class="container newsletter">
+            <div class="row">
+                <div class="col-lg-6">
+                    <p class="newsletter-header">
+                        Let's talk about <br /> your project
+                    </p>
+                    <p class="newsletter-description">
+                        Ready to take it to the next level? Contact us today and find out <br/> how our expertise can help your business grow.
+                    </p>
+                </div>
+                <div class="col-lg-6 newsletter-button">
+                    <div>
+                        <CustomButton link="/our-company">Learn More</CustomButton>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="container">
+            <div class="footer">
+                <div class="logo-container">
+                    <nuxt-link class="" to="/">
+                        <img src="/designo-logo-white.png" alt="Designo Logo" class="mall-logo">
+                    </nuxt-link>
+                </div>
+                <ul class="footer-links">
+                    <li class="footer-item">
+                        <nuxt-link class="footer-link" to="/our-company">Our Company</nuxt-link>
+                    </li>
+                    <li class="footer-item">
+                        <nuxt-link class="footer-link" to="/location">Locations</nuxt-link>
+                    </li>
+                    <li class="footer-item">
+                        <nuxt-link class="footer-link" to="/contact">Contact</nuxt-link>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-address row">
+                <div class="address col-md-5">
+                    <p class="bold-text">Designo Central Office</p>
+                    <p>3886 Wellington Street</p>
+                    <p>Toronto, Ontario M9C 3J5</p>
+                </div>
+                <div class="contact-us col-md-4 bold-text">
+                    <p>Contact us</p>
+                    <p> P : +1 253-863-8967</p>
+                    <p>M : contact@designo.co</p>
+                </div>
+                <div class="social-media-links col-md-3">
+                    <div class="icon">
+                        <font-awesome-icon :icon="['fab', 'square-facebook']" />
+                    </div>
+                    <div class="icon">
+                        <font-awesome-icon :icon="['fab', 'youtube']" />
+                    </div>
+                    <div class="icon">
+                        <font-awesome-icon :icon="['fab', 'square-x-twitter']" />
+                    </div>
+                    <div class="icon">
+                        <font-awesome-icon :icon="['fab', 'pinterest']" />
+                    </div>
+                    <div class="icon">
+                        <font-awesome-icon :icon="['fab', 'instagram']" />
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+</template>
+<script setup>
+import CustomButton from '@/components/CustomButton.vue';
+</script>
+<style  scoped lang="scss"> 
+.footer-wrapper {
+    background-color: black;
+    margin-top: 20rem;
+    padding: 6rem 0 3rem 0;
+    position: relative;
+}
+footer {
+    font-weight: 400;
+    text-transform: uppercase;
+    color: #fff;
+    margin-top: 125px;
+    .bold-text {
+        font-weight: 500;
+    }
+
+    .footer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    
+    .footer-links {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 40px;
+        
+    }
+    .footer-item{
+        margin: 0.5rem 0;
+    }
+    .footer-link {
+        text-decoration: none;
+        color: #fff;
+    }
+    .logo-container {
+        border-bottom: 1px solid #ccc;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-bottom: 40px;
+        img {
+            max-width: 200px;
+        }
+    }
+    
+}
+.footer-address {
+    text-transform: none;
+    padding-top: 2.5rem;
+    .address, .contact-us {
+        text-align: center;
+        margin-bottom: 40px;
+        p {
+            margin-bottom: 0.25rem;
+        }
+    }
+    .social-media-links {
+        display: flex;
+        justify-content: center;
+    }
+}
+.social-media-links {
+    align-items: end;
+    .icon {
+        margin-left: 1rem;
+    }
+}
+.newsletter {
+    position: absolute;
+    top: -25%;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 15px;
+    background-color: #E7816B;
+    padding: 3rem 4rem;
+    color: #fff;
+    text-align: center;
+    max-width: 90%;
+
+    .newsletter-header{
+        font-size: 1.5rem;
+    }
+    .newsletter-button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+}
+@media (min-width: 768px) and (max-width: 1024px){
+    
+}
+@media (min-width: 768px){
+    .newsletter {
+        top: -45%;
+        .newsletter-header {
+            font-size: 2.5rem;
+        }
+        .newsletter-button {
+            margin-top: 20px;
+        } 
+    }
+    footer {
+        .footer {
+            flex-direction: row;
+            justify-content: space-between;
+        }
+        .logo-container {
+            width: auto;
+            padding-bottom: 0;
+            border-bottom: none;
+        }
+        .footer-links {
+            flex-direction: row;
+            margin-top: 0;
+            padding-top: 0;
+        }
+        .footer-item {
+            margin: .5rem;;
+        }
+    }
+    .footer-address {
+        margin-top: 2.5rem;
+        border-top: 1px solid #ccc;
+        .address, .contact-us {
+            margin-bottom: 0;
+            text-align: left;
+        }
+        .social-media-links {
+            justify-content: end;
+        }
+    }
+  
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -68,62 +68,16 @@
             </div>
         </div>
     </div>
-    <div class="container-fluid footer-wrapper">
-            <footer class="container">
-                <div class="footer">
-                    <nuxt-link class="logo-container" to="/">
-                        <img src="/designo-logo-white.png" alt="Designo Logo" class="mall-logo">
-                    </nuxt-link>
-                    <ul class="footer-links">
-                        <li class="footer-item">
-                            <nuxt-link class="footer-link" to="/our-company">Our Company</nuxt-link>
-                        </li>
-                        <li class="footer-item">
-                            <nuxt-link class="footer-link" to="/location">Locations</nuxt-link>
-                        </li>
-                        <li class="footer-item">
-                            <nuxt-link class="footer-link" to="/contact">Contact</nuxt-link>
-                        </li>
-                    </ul>
-                </div>
-                <div class="footer-address row">
-                    <div class="address col-md-5">
-                        <p class="bold-text">Designo Central Office</p>
-                        <p>3886 Wellington Street</p>
-                        <p>Toronto, Ontario M9C 3J5</p>
-                    </div>
-                    <div class="contact-us col-md-4 bold-text">
-                        <p>Contact us</p>
-                        <p> P : +1 253-863-8967</p>
-                        <p>M : contact@designo.co</p>
-                    </div>
-                    <div class="social-media-links col-md-3 d-flex justify-content-end">
-                        <div class="icon">
-                            <font-awesome-icon :icon="['fab', 'square-facebook']" />
-                        </div>
-                        <div class="icon">
-                            <font-awesome-icon :icon="['fab', 'youtube']" />
-                        </div>
-                        <div class="icon">
-                            <font-awesome-icon :icon="['fab', 'square-x-twitter']" />
-                        </div>
-                        <div class="icon">
-                            <font-awesome-icon :icon="['fab', 'pinterest']" />
-                        </div>
-                        <div class="icon">
-                            <font-awesome-icon :icon="['fab', 'instagram']" />
-                        </div>
-                    </div>
-                </div>
-            </footer>
-
-        </div>
+    <FooterComponent />
+   
 
 </template>
 <script setup>
+
 import CustomButton from '@/components/CustomButton.vue';
 import Navigation from '@/components/Navigation.vue';
 import FeaturedItems from '@/components/FeaturedItems.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
 
 useHead({
     title: null
@@ -236,90 +190,4 @@ useHead({
         text-align: center;
     }
 }
-.footer-wrapper {
-    background-color: black;
-    margin-top: 6rem;
-    padding: 6rem 0 3rem 0;
-}
-footer {
-    // display: flex;
-    // justify-content: center;
-    // align-items: center;
-    font-weight: 400;
-    text-transform: uppercase;
-    color: #fff;
-    .bold-text {
-        font-weight: 500;
-    }
-
-    .footer {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    
-    .footer-links {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        
-    }
-    .footer-item{
-        margin: 0.5rem 0;
-    }
-    .footer-link {
-        text-decoration: none;
-        color: #fff;
-    }
-    .logo-container {
-        img {
-            max-width: 200px;
-        }
-    }
-    
-}
-.footer-address {
-    text-transform: none;
-    border-top: 1px solid #ccc;
-    margin-top: 2.5rem;
-    padding-top: 2.5rem;
-    .address, .contact-us {
-        p {
-            margin-bottom: 0.25rem;
-        }
-    }
-}
-.social-media-links {
-    align-items: end;
-    .icon {
-        margin-left: 1rem;
-    }
-}
-
-@media (min-width: 769px){
-    footer {
-        // display: flex;
-        // justify-content: space-around;
-        .footer {
-            flex-direction: row;
-            justify-content: space-between;
-        }
-        .footer-links {
-            flex-direction: row;
-            
-        }
-        .footer-item {
-            margin: 0 1rem;
-        }
-
-        .social-media-links {
-            align-items: center !important;
-        }
-    }
-   
-}
-
 </style>
