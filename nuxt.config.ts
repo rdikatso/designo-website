25fc73de69807cb8e567abeac710b4508e019290
Dikatso <rdikatso@gmail.com>
2025-08-30 17:58:51 -0400

    [Dikatso] remove api key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,17 +17,5 @@ export default defineNuxtConfig({
     '~/plugins/fontawesome.js',
     '~/plugins/vee-validate.js'
   ],
-  runtimeConfig: {
-    public: {
-      FB_API_KEY: "AIzaSyBUCsa4CHnjTnUm8Joq7p1emYDcxdYVNE8",
-      FB_AUTH_DOMAIN: "designo-product-catalog.firebaseapp.com",
-      FB_PROJECT_ID: "designo-product-catalog",
-      FB_STORAGE_BUCKET: "designo-product-catalog.firebasestorage.app",
-      FB_MESSAGING_SENDER_ID: "165295827987",
-      FB_APP_ID: "1:165295827987:web:2da998f65d4943a1fe63e6",
-      FB_MEASUREMENT_ID: "G-Y3V4L9SEYF",
-    },
-  },
-
   modules: ["@nuxtjs/leaflet"]
 })
